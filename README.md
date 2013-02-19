eba694963edab47c2ca1f94283ff9a750753d668
Julian Simpson <simpsonjulian@gmail.com>
2013-02-19 13:46:27 +1300

    Now lists uncommented issues.
    You'll need to either hack the source, or introduce a way to bring in authentication from outside. Or, you can use GitHub without authentication, and use a *lot* of sleeps.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-* gem install bundler  
-* bundle install  
+* make sure you have Ruby 1.9
+* gem install bundler
+* bundle install
+* give it your credentials at the top of ./issues.rb (HINT: remove them before you commit to GitHub)
 * bundle exec ./issues.rb  
--- issues.rb
+++ issues.rb
@@ -3,15 +3,47 @@ require 'rubygems'
 require 'bundler'
 require 'github_api'
 
+CREDENTIALS = PUT YOUR BASIC AUTH CREDENTIALS HERE AND TRUST THAT THE LIBRARY AUTHOR IS NOT HARVESTING THEM AND YOUR ORGANS
+# e.g. CREDENTIALS='foo:bar'
 
-#issues = Github::Issues.new
-#puts issues.list(:org => 'neo4j')
+@github = Github.new(basic_auth: CREDENTIALS)
 
-github = Github.new
-github.issues.list(:user => 'neo4j', :repo => 'neo4j').each_page do |page|
-  page.each do |issue|
-    puts issue.title
+def imported?(comments)
+  comments.select { |comment| comment.body.match(/This issue was migrated from /) }.length >= 1
+end
+
+def pr?(issue)
+  issue.respond_to?(:pull_request)
+end
+
+def comments(issue)
+  @github.issues.comments.all('neo4j', 'neo4j', issue_id: issue.number)
+end
+
+def commented?(comments)
+  imported = imported?(comments)
+  if imported && comments.length == 1
+    false
+  elsif !imported && comments.length == 0
+    return false
+  else
+    true
   end
 end
+
+begin
+  @github.issues.list(:user => 'neo4j', :repo => 'neo4j').each_page do |page|
+    page.each do |issue|
+      next if pr?(issue)
+      comments = comments(issue)
+      printf("%3s %-60.60s %-10s\n", issue.number, issue.title, issue.user.login) unless commented?(comments)
+      sleep 1 # be kind to github
+    end
+  end
+rescue Github::Error::Forbidden => e
+  puts "It looks like you displeased GitHub: #{e.message}"
+end
+
+
 
 
